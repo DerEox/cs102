1243396bd36ac87a234dec5967f1829cda1b4d3b
Yulia <zherebtsova.yu@inbox.ru>
2019-09-25 14:04:02 +0300

    '1lab.py'

---
+++ 1lab.py
@@ -0,0 +1,23 @@
+# max of 2 numbers
+
+
+
+# a = input ("Enter a: ")
+# b = input ("Enter b: ")
+# max = int
+
+# if a < b:
+# 	max = b
+# else:
+# 	max = a
+
+# print('max = ', max)
+
+
+
+# # a program which outputs only the numbers with even index (i)  
+
+age = 26
+name = 'Swaroop'
+print('Возраст {0} -- {1} лет.'.format(name, age))
+print('Почему {0} забавляется с этим Python?'.format(name))
