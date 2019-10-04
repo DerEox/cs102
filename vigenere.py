f8b9e310464a230eebe67e7a98bdbf4128a304e9
Yulia <zherebtsova.yu@inbox.ru>
2019-10-04 23:17:16 +0300

    Добавлена функция encrypt_vigenere

--- vigenere.py
+++ vigenere.py
@@ -1,13 +1,22 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
-    # PUT YOUR CODE HERE
+    key = list(keyword)
+    ciphertext = []
+    while len(key)<len(plaintext):
+        for i in keyword:
+            if len(key)<len(plaintext):
+                key.append(i)        
+    if keyword.isupper():
+        for l, k in zip(plaintext, key):
+            if (ord(l)+ord(k))-65 >90:
+               l = chr((ord(l))-26)
+            c = (chr(ord(l) + (ord(k)-65)))
+            ciphertext.append(c)
+    elif keyword.islower():
+        for l, k in zip(plaintext, key):
+            if (ord(l)+ord(k))-97 >122:
+                l = chr((ord(l))-26)
+            c = (chr(ord(l) + (ord(k)-97)))
+            ciphertext.append(c)
     return ciphertext
 
 
